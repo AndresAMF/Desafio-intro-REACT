0550f9fe0b7165b4f8b4cf02b4be4562196c17c6
AndresAMF <andres.1991.munoz@gmail.com>
2022-08-02 01:52:18 -0400

    se eliminan css vacios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import './Footer.css';
-
 
 const Footer = () => {
-
-        return (
-        <div className="footerContainer">
-            <h5 className="footerText">All rigths reserved.</h5>
-        </div>
-    )
-}
+  return (
+    <div className="footerContainer">
+      <h5 className="footerText">All rigths reserved.</h5>
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import './Header.css';
-
-
 
 const Header = (props) => {
-        return (
-        <h1 className="title">{props.title}</h1>
-    )
-}
+  return <h1 className="title">{props.title}</h1>;
+};
 
 export default Header;
